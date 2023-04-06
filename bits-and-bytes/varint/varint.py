f0b55f8a7523f449439007ec798e7e8953317459
hyy <yh8953@utexas.edu>
2023-04-06 12:49:07 -0500

    add decoder for Protobuf varint

--- bits-and-bytes/varint/varint.py
+++ bits-and-bytes/varint/varint.py
@@ -20,11 +20,38 @@ def encode(n):
         out.append(part)
     return bytes(out)
 
-# Reading in the data
-# 'rb' - read in the input file and interpret as binary
-with open('150.uint64', 'rb') as f:
-    # Convert the bytes to integer representation
-    # '>Q' - big endian 64-bit integer code for unpack
-    # see help(unpack) for more info
-    n = struct.unpack('>Q', f.read())[0] # unpack always returns a sequence
-    print(encode(n))
+
+def decode(var_n):
+    '''
+    for b in var_n (right to left):
+        shift accumulator left by 7
+        discard msb
+        accumulate b
+    '''
+    n = 0
+    for b in reversed(var_n):
+        n <<= 7
+        b &= 0b01111111 # 0x7f
+        n += b
+    return n
+
+
+if __name__ == '__main__':
+    cases = (
+        ('1.uint64', b'\x01'),
+        ('150.uint64', b'\x96\x01'),
+        ('maxint.uint64', b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\x01'),
+    )
+
+    for fname, expectation in cases:
+        # Reading in the data
+        # 'rb' - read in the input file and interpret as binary
+        with open(fname, 'rb') as f:
+            # Convert the bytes to integer representation
+            # '>Q' - big endian 64-bit integer code for unpack
+            # see help(unpack) for more info
+            n = struct.unpack('>Q', f.read())[0] # unpack always returns a sequence
+            # testing encoder
+            assert encode(n) == expectation
+            assert decode(encode(n)) == n
+    print('ok')
