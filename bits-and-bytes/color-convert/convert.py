2cd7f8494904117eab19f87614d8749a733d7efc
hyy <yh8953@utexas.edu>
2023-04-21 11:10:56 -0500

    converter implementation for advanced cases

--- bits-and-bytes/color-convert/convert.py
+++ bits-and-bytes/color-convert/convert.py
@@ -19,9 +19,23 @@ def byte_to_dec(byte_str):
 # Input: Regex object
 # Output: String representation of decimal value
 def hex_to_rgb(r):
-    hex_str = r.group(1)
+    hex_str = r.group(1).lower()
+    # normalize to non-abbreviated form
+    if len(hex_str) in {3, 4}:
+        hex_str = ''.join(x + x for x in hex_str)
+    # computed decimal form of (R, G, B)
     dec_ints = [byte_to_dec(hex_str[i: i + 2]) for i in (0, 2, 4)]
-    return 'rgb(%s)' % (','.join(str(d) for d in dec_ints))
+    # maybe compute alpha channel
+    if len(hex_str) == 6:
+        label = "rgb"
+    elif len(hex_str) == 8:
+        label = "rgba"
+        dec_ints.append('/')
+        dec_ints.append("{:.5f}".format(byte_to_dec(hex_str[6:]) / 255))
+    else:
+        return f'#{hex_str}' # not the best way to validate
+
+    return f'{label}({" ".join(str(d) for d in dec_ints)})'
 
 output = re.sub(r'\#([0-9a-fA-F]+)', hex_to_rgb, sys.stdin.read())
 sys.stdout.write(output)
